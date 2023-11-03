7815b9c9d1c4b077aa2f06a682eb13beeae658c7
Daniel Reid <dan.reid3005@gmail.com>
2023-11-03 11:09:20 +0000

    connecting mongodb

--- footballApp/api/server.js
+++ footballApp/api/server.js
@@ -6,3 +6,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/FootballDB")
+.then(() => console.log("Connected to DB"))
+.catch(console.error);
+
+app.listen(5000, () => console.log("Server started on port 5000"));
